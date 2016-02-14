9c7e3a1d3f11a33a09bb801ce7d15c0cb685d019
Jeff Bayes <jbayes92@gmail.com>
2016-02-13 21:01:39 -0800

    changes are a comin

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var bodyParser = require('body-parser');
 
 var MY_SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 var slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
+// THIS IS BAD NAMESPACING. FIX IT LATER.
+var Slack = require('slack-node');
+// REMOVE BEFORE PRODUCTION. IT HAS SEEN THE LIGHT OF DAY.
+var apiToken = "xoxp-21255113202-21255543414-21308331154-232305f633";
+var SlackAPI = new Slack(apiToken);
 
 var UserApp = require('userapp');
 UserApp.initialize({
@@ -22,8 +27,6 @@ app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodi
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-
-
 //routes
 
 app.get('/help/:id', function(req, res) {
@@ -32,26 +35,123 @@ app.get('/help/:id', function(req, res) {
 	var requestID = req.params.id;
 	console.log(requestID);
 	var requestArray = requestID.split('-');
-	console.log(requestArray[0])
+	console.log(requestArray[0]);
+	console.log(requestArray[1]);
 	// REPLACE BEFORE PRODUCTION. IT HAS SEEN THE LIGHT OF DAY.
 	UserApp.setToken("x-WrIq6UQC20sI2a-a8bMg");
 	UserApp.User.get({"user_id": requestArray[0]}, function(error, result){
-		if(error){
+		if(!error){
+			console.log(result[0])
+			var userEmail = result[0]['email']
+			var userPhone = result[0]['properties']['phone']['value'];
+			var userRequests = JSON.parse(result[0]['properties']['requests']['value']);
+			console.log(userRequests)
+			var theRequest = userRequests.map(function(item){
+				if (item['id'] == requestArray[1]){
+					res.render('help', {phone: userPhone, email: userEmail, info: item['text']});
+				} else { return item; }
+			})
+		} else {
 			console.log(error);
 			console.log("Uh oh, this doesn't work! /help/:id");
-		} else {
-			var userPhone = result[0]['properties']['phone']['value'];
-			res.render('help', {phone: userPhone});
 		}
 
 	});
-	// Parse into userID and request number.
-	// Search userapp for userid
-	// find request number
-	// fetch info from relevant request
-	// fetch info about requester
-
 });
+
+function cleanArray(actual) {
+  var newArray = new Array();
+  for (var i = 0; i < actual.length; i++) {
+    if (actual[i]) {
+      newArray.push(actual[i]);
+    }
+  }
+  return newArray;
+}
+
+app.post('/help/close/:id', function(req, res) {
+	var requestID = req.params.id;
+	console.log(requestID);
+	var requestArray = requestID.split('-');
+	console.log(requestArray[0]);
+	console.log(requestArray[1]);
+	// REPLACE BEFORE PRODUCTION. IT HAS SEEN THE LIGHT OF DAY.
+	UserApp.setToken("x-WrIq6UQC20sI2a-a8bMg");
+	UserApp.User.get({"user_id": requestArray[0]}, function(error, result){
+		if(!error) {
+			console.log(result[0])
+			var userEmail = result[0]['email']
+			var userPhone = result[0]['properties']['phone']['value'];
+			var userRequests = JSON.parse(result[0]['properties']['requests']['value']);
+			console.log(userRequests)
+			var removeRequests = userRequests.map(function(item){
+				if (item['id'] == requestArray[1]){
+					// Don't return it
+				} else { return item; }
+			});
+			var updatedRequests = cleanArray(removeRequests);
+			result[0].properties.requests.value = JSON.stringify(updatedRequests);
+			UserApp.User.save(result[0], function(error, result){
+				console.log(error, result);
+				console.log("REQUEST ID: ", requestID);
+				// BRING BACK TIMEOUT FOR PRODUCTION. A QUICK RESPONSE DOESN'T WORK FOR SLACK.
+				setTimeout(function() {
+					SlackAPI.api('search.messages', { 
+						query: requestID
+					}, function(err, resp){
+						console.log(resp);
+						var match = resp['messages']['matches'][0];
+						console.log(match);
+						SlackAPI.api('chat.delete', {
+							ts: match['ts'],
+							channel: match['channel']['id']
+						}, function(err, response){
+							console.log(response);
+							var messageText = "Recent request #" + requestArray[1] + " has been fulfilled. Thank you!";
+							var fancyMessage = {
+				  				channel: '#shelter',
+							  	icon_emoji: ':house:',
+							  	username: "Request Accepted!",
+								attachments: [{
+									fallback: messageText,
+									title: "Request Accepted!",
+									text: messageText,
+									color: "#7CD197"
+								}]
+							}
+
+							var errorHandler = function(err){
+								if (err) {
+									console.log('API error:', err);
+								} else {
+									console.log('Message received!');
+									res.redirect('/');
+								}
+							};
+							slack.send(fancyMessage, errorHandler);
+						})
+						console.log(match);
+					});
+				}, 14000)
+			});
+
+
+			// res.render('help', {phone: userPhone, email: userEmail});
+		} else {
+			console.log(error);
+			console.log("Uh oh, this doesn't work! /help/close/:id");
+		}
+
+	});
+
+	// result[0].properties.requests.value = JSON.stringify(requests);
+
+	// console.log(result[0].properties.requests.value);
+
+	// UserApp.User.save(result[0], function(error, result){
+	// 	console.log(error, result);
+	// })
+})
 
 
 app.post('/postToSlack/:key?', function(req, res) {
@@ -100,7 +200,7 @@ app.post('/postToSlack/:key?', function(req, res) {
 					title: "<!channel>: New request for " + service + ".",
 					title_link: requestURL,
 					text: basicText,
-					color: "#7CD197"
+					color: "#38B0DE"
 				}]
 			}
 
